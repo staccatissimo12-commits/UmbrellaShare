06e467cb1b8788b4cb8b7f3e401d6341dfbca7ab
staccatissimo12-commits <staccatissimo12@gmail.com>
2025-10-01 17:31:38 +0900

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -60,7 +60,8 @@ app.use((req, res, next) => {
   // Other ports are firewalled. Default to 5000 if not specified.
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
-  const port = parseInt(process.env.PORT || '5000', 10);
+  const port = process.env.PORT || '5000';
+  const server = app.listen(PORT, '0.0.0.0');
   server.listen({
     port,
     host: "0.0.0.0",
